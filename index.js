46bb60c5813a6bbcc7bcf7d351432c07c131ee52
hawraa473 <hawraamuayad473@gmail.com>
2024-02-12 23:10:54 +0300

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-function showYoungestStudent(students) { }
+const students = [
+    { name: 'Hawraa' , age: 21 },
+    { name: 'Amna' , age: 22 } ,
+    { name: 'Fatima' , age: 19 } ,
+] ; 
 
-module.exports = showYoungestStudent;
+function getYoungestStudents(students) {
+    let youngestStudent = { age: students[0].age, name: "" };
+    for (let i = 0; i < students.length; i++) {
+        const student = students[i];
+        if (student.age < youngestStudent.age) {
+            youngestStudent = student;
+        }
+    }
+    return youngestStudent;
+}
+
+console.log(getYoungestStudents(students));
+
+//module.exports = showYoungestStudent;
